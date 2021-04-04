4d8570003139494fcff5cf848a9abd91e1cb27ef
Herbert Treis Neto <htnetooo@gmail.com>
2021-04-03 22:12:17 -0300

    fix: loader name typo cancels the resolver chain

--- webpack.rules.js
+++ webpack.rules.js
@@ -8,7 +8,7 @@ module.exports = [
     test: /\.(m?js|node)$/,
     parser: { amd: false },
     use: {
-      loader: '@vercel/webpack-asset-relocator-loade',
+      loader: '@vercel/webpack-asset-relocator-loader',
       options: {
         outputAssetBase: 'native_modules',
       },
