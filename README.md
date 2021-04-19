4c02d31c5aea2e704ef87d83a3fa4c6c4596ff95
Jeff Robbins <jeff@jjeff.com>
2021-04-19 10:48:00 -0400

    CSS + docs

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # electron-bytenode-example
 
-Example code using [Bytenode](https://github.com/OsamaAbbas/bytenode) to compile both the render and main processes in an Electron app.
+Example code using [Bytenode](https://github.com/OsamaAbbas/bytenode) + [Bytenode Webpack Plugin](https://github.com/herberttn/bytenode-webpack-plugin) to compile both the render and main processes in an Electron app.
+
+Clone or download this repository and then:
 
 ```bash
 npm install
 npm start
 ```
+
+This example uses Herbert Treis Neto's [Bytenode Webpack Plugin](https://github.com/herberttn/bytenode-webpack-plugin) along with Webpack and [Electron Forge](https://www.electronforge.io) to bundle and package the application.
--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,6 @@
-// Modules to control application life and create native browser window
 import { app, BrowserWindow } from 'electron'
-import isElectron from 'is-electron'
-
-// const path = require('path')
-
-// console.log('Greetings from main.js')
-
-if (isElectron()) {
-  console.log('I am Electron!')
-}
 
 function createWindow() {
-  // Create the browser window.
-
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
@@ -20,20 +8,18 @@ function createWindow() {
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
+      // Electron Forge entry point ⤵
       preload: MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY
     }
   })
 
-  // and load the index.html of the app.
+  // Electron Forge entry point ⤵
   mainWindow.loadURL(MAIN_WINDOW_WEBPACK_ENTRY)
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.whenReady().then(createWindow)
 
 // Quit when all windows are closed.
@@ -48,6 +34,3 @@ app.on('activate', function () {
   // dock icon is clicked and there are no other windows open.
   if (BrowserWindow.getAllWindows().length === 0) createWindow()
 })
-
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
