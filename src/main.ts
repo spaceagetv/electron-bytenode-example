986ace5f15a1bb9f853baf94ebf0babc59898b2b
Darrel O'Pry <darrel.opry@spry-group.com>
2022-10-25 00:45:42 -0400

    feat: add sentry to demostrate and external modules being included.

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,12 @@
-import { app, BrowserWindow } from 'electron'
+// Second, setup sentry to capture errors for support and maintenance
+import * as Sentry from "@sentry/electron/main";
+Sentry.init({
+  dsn: "https://d7e0056c96674f52ab2c8d8fc2023355@o4504042369318912.ingest.sentry.io/4504042372399104",
+  // not really trying to send any traffic, just want valid sentry config.
+  tracesSampleRate: 0.00000000001,
+});
+
+import { app, BrowserWindow } from "electron";
 
 function createWindow() {
   // Create the browser window.
@@ -21,17 +29,17 @@ function createWindow() {
   // mainWindow.webContents.openDevTools()
 }
 
-app.whenReady().then(createWindow)
+app.whenReady().then(createWindow);
 
 // Quit when all windows are closed.
-app.on('window-all-closed', function () {
+app.on("window-all-closed", function () {
   // On macOS it is common for applications and their menu bar
   // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') app.quit()
-})
+  if (process.platform !== "darwin") app.quit();
+});
 
-app.on('activate', function () {
+app.on("activate", function () {
   // On macOS it's common to re-create a window in the app when the
   // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) createWindow()
-})
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
--- webpack.main.config.js
+++ webpack.main.config.js
@@ -1,17 +1,15 @@
-const { BytenodeWebpackPlugin } = require('@herberttn/bytenode-webpack-plugin')
-const rules = require('./webpack.rules')
-
+const { BytenodeWebpackPlugin } = require("@herberttn/bytenode-webpack-plugin");
+const rules = require("./webpack.rules");
 
 /** @type {import(‘@types/webpack’).Configuration} */
 module.exports = {
-  entry: './src/main.ts',
+  entry: { index: "./src/main.ts" },
   output: {
-    devtoolModuleFilenameTemplate: '[absolute-resource-path]',
+    filename: "[name].js",
+    devtoolModuleFilenameTemplate: "[absolute-resource-path]",
   },
   module: {
     rules,
   },
-  // plugins: [
-  //   new BytenodeWebpackPlugin({ compileForElectron: true }),
-  // ],
-}
+  plugins: [new BytenodeWebpackPlugin({ compileForElectron: true })],
+};
